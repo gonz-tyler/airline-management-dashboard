27be37a5b7986eef3a819ca5b9baf67073363a56
gonz-tyler <gonzaloescolartyler@gmail.com>
2024-05-07 11:30:07 +0100

    started creation of views for each table

--- website/urls.py
+++ website/urls.py
@@ -6,16 +6,89 @@ urlpatterns = [
     #path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
+
+    #-- planes --#
     path('planes/', views.view_planes, name='view_planes'),
     path('planes/<int:pk>', views.view_plane, name='view_plane'),
     path('delete_plane/<int:pk>', views.delete_plane, name='delete_plane'),
+    path('add_plane', views.add_plane, name='add_plane'),
+
+    #-- cities --#
+    path('cities/', views.view_cities, name='view_cities'),
+    path('cities/<str:pk>', views.view_city, name='view_city'),
+    path('delete_city/<str:pk>', views.delete_city, name='delete_city'),
+    path('add_city', views.add_city, name='add_city'),
+
+    #-- flight crews --#
+    path('crews/', views.view_crews, name='view_crews'),
+    path('crews/<int:pk>', views.view_crew, name='view_crew'),
+    path('delete_crew/<int:pk>', views.delete_crew, name='delete_crew'),
+    path('add_crew', views.add_crew, name='add_crew'),
+
+    #-- flights --#
     path('flights/', views.view_flights, name='view_flights'),
     path('flights/<int:pk>', views.view_flight, name='view_flight'),
     path('delete_flight/<int:pk>', views.delete_flight, name='delete_flight'),
+    path('add_flight', views.add_flight, name='add_flight'),
 
-    path('cities/', views.view_cities, name='view_cities'),
-    path('cities/<str:pk>', views.view_city, name='view_city'),
+    #-- passenger address --#
+    path('passenger_addresses/', views.view_passenger_addresses, name='view_passenger_addresses'),
+    path('passenger_addresses/<int:pk>', views.view_passenger_address, name='view_passenger_address'),
+    path('delete_passenger_address/<int:pk>', views.delete_passenger_address, name='delete_passenger_address'),
+    path('add_passenger_address', views.add_passenger_address, name='add_passenger_address'),
+
+    #-- passenger booking --#
     path('pilots/', views.view_pilots, name='view_pilots'),
     path('pilots/<int:pk>', views.view_pilot, name='view_pilot'),
+    path('delete_pilot/<int:pk>', views.delete_pilot, name='delete_pilot'),
+    path('add_pilot', views.add_pilot, name='add_pilot'),
+
+    #-- passenger phone --#
+    path('passenger_phones/', views.view_passenger_phones, name='view_passenger_phones'),
+    path('passenger_phones/<int:pk>', views.view_passenger_phone, name='view_passenger_phone'),
+    path('delete_passenger_phone/<int:pk>', views.delete_passenger_phone, name='delete_passenger_phone'),
+    path('add_passenger_phone', views.add_passenger_phone, name='add_passenger_phone'),
+
+    #-- passenger --#
+    path('passengers/', views.view_passengers, name='view_passengers'),
+    path('passenger/<int:pk>', views.view_pilot, name='view_passenger'),
+    path('delete_passenger/<int:pk>', views.delete_pilot, name='delete_passenger'),
+    path('add_passenger', views.add_passenger, name='add_passenger'),
+
+    #-- pilot typeratings --#
+    path('pilot_typeratings/', views.view_pilot_typeratings, name='view_pilot_typeratings'),
+    path('pilot_typeratings/<int:pk>', views.view_pilot_typerating, name='view_pilot_typerating'),
+    path('delete_pilot_typerating/<int:pk>', views.delete_pilot_typerating, name='delete_pilot_typerating'),
+    path('add_pilot_typerating', views.add_pilot_typerating, name='add_pilot_typerating'),
+
+    #-- pilots --#
+    path('pilots/', views.view_pilots, name='view_pilots'),
+    path('pilots/<int:pk>', views.view_pilot, name='view_pilot'),
+    path('delete_pilot/<int:pk>', views.delete_pilot, name='delete_pilot'),
+    path('add_pilot', views.add_pilot, name='add_pilot'),
+
+    #-- staff addresses --#
+    path('staff_addresses/', views.view_staff_addresses, name='view_staff_addresses'),
+    path('staff_addresses/<int:pk>', views.view_staff_address, name='view_staff_address'),
+    path('delete_staff_address/<int:pk>', views.delete_staff_address, name='delete_staff_address'),
+    path('add_staff_address', views.add_staff_address, name='add_staff_address'),
+
+    #-- staff phones --#
+    path('staff_phones/', views.view_staff_phones, name='view_staff_phones'),
+    path('staff_phones/<int:pk>', views.view_staff_phone, name='view_staff_phone'),
+    path('delete_staff_phone/<int:pk>', views.delete_staff_phone, name='delete_staff_phone'),
+    path('add_staff_phone', views.add_staff_phone, name='add_staff_phone'),
+    
+    #-- staff --#
+    path('staff/', views.view_staff, name='view_staff'),
+    path('staff/<int:pk>', views.view_staff_member, name='view_staff_members'),
+    path('delete_staff/<int:pk>', views.delete_staff_member, name='delete_staff'),
+    path('add_staff', views.add_staff_member, name='add_staff'),
+    
+    #-- stretch --#
+    path('stretches/', views.view_stretches, name='view_stretches'),
+    path('stretches/<int:pk>', views.view_stretch, name='view_stretch'),
+    path('delete_stretch/<int:pk>', views.delete_stretch, name='delete_stretch'),
+    path('add_stretch', views.add_stretch, name='add_stretch'),
 
 ]
--- website/views.py
+++ website/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from .forms import SignUpForm
 from .models import *
 
+#-- Render Home Page --#
 def home(request):
     flights = Flight.objects.all()
     cities = City.objects.all()
@@ -23,12 +24,13 @@ def home(request):
     else:
         return render(request, 'home.html', {'flights': flights})
 
+#-- Logout User --#
 def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out...")
     return redirect('home')
 
-
+#-- Register User --#
 def register_user(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -46,60 +48,7 @@ def register_user(request):
         return render(request, 'register.html', {'form':form})
     return render(request, 'register.html', {'form':form})
 
-def view_flights(request):
-    if request.user.is_authenticated:
-        flights = Flight.objects.all()
-        return render(request, 'home.html', {'flights': flights})
-
-def view_flight(request, pk):
-    if request.user.is_authenticated:
-        #look up flight
-        flight = Flight.objects.get(FLIGHTNUM=pk)
-        return render(request, 'flight_details.html', {'flight':flight})
-    else:
-        messages.success(request, 'You must be logged in to view this page.')
-        return redirect('home')
-    
-def delete_flight(request, pk):
-    if request.user.is_authenticated:
-        delete_it = Flight.objects.get(FLIGHTNUM=pk)
-        flight_number = delete_it.FLIGHTNUM
-        delete_it.delete()
-        messages.success(request, f"Flight {flight_number} Deleted Successfully")
-        flight_number = None
-        return redirect('home')
-    else:
-        messages.success(request, "You must be logged in to perform this action.")
-        return redirect('home')
-    
-def view_cities(request):
-    if request.user.is_authenticated:
-        cities = City.objects.all()
-        return render(request, 'cities.html', {'cities': cities})
-
-def view_city(request, pk):
-    if request.user.is_authenticated:
-        #look up city
-        city = City.objects.get(CITYCODE=pk)
-        return render(request, 'city_details.html', {'city':city})
-    else:
-        messages.success(request, 'You must be logged in to view this page.')
-        return redirect('home')
-    
-def view_pilots(request):
-    if request.user.is_authenticated:
-        pilots = Pilot.objects.all()
-        return render(request, 'pilots.html', {'pilots': pilots})
-
-def view_pilot(request, pk):
-    if request.user.is_authenticated:
-        #look up plane
-        pilot = Pilot.objects.get(PILOTNUM=pk)
-        return render(request, 'pilot_details.html', {'pilot':pilot})
-    else:
-        messages.success(request, 'You must be logged in to view this page.')
-        return redirect('home')
-
+#-- Planes --#
 def view_planes(request):
     if request.user.is_authenticated:
         planes = Airplane.objects.all()
@@ -125,3 +74,290 @@ def delete_plane(request, pk):
     else:
         messages.success(request, "You must be logged in to perform this action.")
         return redirect('home')
+    
+def add_plane(request):
+    pass
+
+#-- Cities --#    
+def view_cities(request):
+    if request.user.is_authenticated:
+        cities = City.objects.all()
+        return render(request, 'cities.html', {'cities': cities})
+
+def view_city(request, pk):
+    if request.user.is_authenticated:
+        #look up city
+        city = City.objects.get(CITYCODE=pk)
+        return render(request, 'city_details.html', {'city':city})
+    else:
+        messages.success(request, 'You must be logged in to view this page.')
+        return redirect('home')
+    
+def delete_city(request, pk):
+    pass
+
+def add_city(request):
+    pass
+
+#-- Flight Crews --#    
+def view_crews(request):
+    if request.user.is_authenticated:
+        crews = FlightCrew.objects.all()
+        return render(request, 'crews.html', {'crews': crews})
+
+def view_crew(request, pk):
+    if request.user.is_authenticated:
+        #look up city
+        crew = FlightCrew.objects.get(EMPNUM=pk)
+        return render(request, 'crew_details.html', {'crew':crew})
+    else:
+        messages.success(request, 'You must be logged in to view this page.')
+        return redirect('home')
+    
+def delete_crew(request, pk):
+    pass
+
+def add_crew(request):
+    pass
+
+#-- Flights --#
+def view_flights(request):
+    if request.user.is_authenticated:
+        flights = Flight.objects.all()
+        return render(request, 'home.html', {'flights': flights})
+
+def view_flight(request, pk):
+    if request.user.is_authenticated:
+        #look up flight
+        flight = Flight.objects.get(FLIGHTNUM=pk)
+        return render(request, 'flight_details.html', {'flight':flight})
+    else:
+        messages.success(request, 'You must be logged in to view this page.')
+        return redirect('home')
+    
+def delete_flight(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Flight.objects.get(FLIGHTNUM=pk)
+        flight_number = delete_it.FLIGHTNUM
+        delete_it.delete()
+        messages.success(request, f"Flight {flight_number} Deleted Successfully")
+        flight_number = None
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to perform this action.")
+        return redirect('home')
+
+def add_flight(request):
+    return render(request, 'add_flight.html')
+
+#-- Passenger Addresses --#    
+def view_passenger_addresses(request):
+    if request.user.is_authenticated:
+        passenger_addresses = PassengerAddress.objects.all()
+        return render(request, 'passenger_addresses.html', {'passenger_addresses': passenger_addresses})
+
+def view_passenger_address(request, pk):
+    if request.user.is_authenticated:
+        #look up city
+        city = City.objects.get(CITYCODE=pk)
+        return render(request, 'city_details.html', {'city':city})
+    else:
+        messages.success(request, 'You must be logged in to view this page.')
+        return redirect('home')
+    
+def delete_passenger_address(request, pk):
+    pass
+
+def add_passenger_address(request):
+    pass
+
+#-- Passenger Bookings --#    
+def view_passenger_bookings(request):
+    if request.user.is_authenticated:
+        cities = City.objects.all()
+        return render(request, 'cities.html', {'cities': cities})
+
+def view_passenger_booking(request, pk):
+    if request.user.is_authenticated:
+        #look up city
+        city = City.objects.get(CITYCODE=pk)
+        return render(request, 'city_details.html', {'city':city})
+    else:
+        messages.success(request, 'You must be logged in to view this page.')
+        return redirect('home')
+    
+def delete_passenger_booking(request, pk):
+    pass
+
+def add_passenger_booking(request):
+    pass
+
+#-- Passenger Phones --#    
+def view_passenger_phones(request):
+    if request.user.is_authenticated:
+        cities = City.objects.all()
+        return render(request, 'cities.html', {'cities': cities})
+
+def view_passenger_phone(request, pk):
+    if request.user.is_authenticated:
+        #look up city
+        city = City.objects.get(CITYCODE=pk)
+        return render(request, 'city_details.html', {'city':city})
+    else:
+        messages.success(request, 'You must be logged in to view this page.')
+        return redirect('home')
+    
+def delete_passenger_phone(request, pk):
+    pass
+
+def add_passenger_phone(request):
+    pass
+
+#-- Passengers --#    
+def view_passengers(request):
+    if request.user.is_authenticated:
+        cities = City.objects.all()
+        return render(request, 'cities.html', {'cities': cities})
+
+def view_passenger(request, pk):
+    if request.user.is_authenticated:
+        #look up city
+        city = City.objects.get(CITYCODE=pk)
+        return render(request, 'city_details.html', {'city':city})
+    else:
+        messages.success(request, 'You must be logged in to view this page.')
+        return redirect('home')
+    
+def delete_passenger(request, pk):
+    pass
+
+def add_passenger(request):
+    pass
+
+#-- Pilot Typeratings --#    
+def view_pilot_typeratings(request):
+    if request.user.is_authenticated:
+        cities = City.objects.all()
+        return render(request, 'cities.html', {'cities': cities})
+
+def view_pilot_typerating(request, pk):
+    if request.user.is_authenticated:
+        #look up city
+        city = City.objects.get(CITYCODE=pk)
+        return render(request, 'city_details.html', {'city':city})
+    else:
+        messages.success(request, 'You must be logged in to view this page.')
+        return redirect('home')
+    
+def delete_pilot_typerating(request, pk):
+    pass
+
+def add_pilot_typerating(request):
+    pass
+
+#-- Pilots --#    
+def view_pilots(request):
+    if request.user.is_authenticated:
+        pilots = Pilot.objects.all()
+        return render(request, 'pilots.html', {'pilots': pilots})
+
+def view_pilot(request, pk):
+    if request.user.is_authenticated:
+        # Look up pilot
+        pilot = Pilot.objects.get(PILOTNUM=pk)
+        # Get all type ratings for the pilot
+        typeratings = PilotTypeRating.objects.filter(PILOTNUM=pilot)
+        return render(request, 'pilot_details.html', {'pilot': pilot, 'typeratings': typeratings})
+    else:
+        messages.success(request, 'You must be logged in to view this page.')
+        return redirect('home')
+    
+def delete_pilot(request, pk):
+    pass
+
+def add_pilot(request):
+    pass
+
+#-- Staff Addresses --#    
+def view_staff_addresses(request):
+    if request.user.is_authenticated:
+        cities = City.objects.all()
+        return render(request, 'cities.html', {'cities': cities})
+
+def view_staff_address(request, pk):
+    if request.user.is_authenticated:
+        #look up city
+        city = City.objects.get(CITYCODE=pk)
+        return render(request, 'city_details.html', {'city':city})
+    else:
+        messages.success(request, 'You must be logged in to view this page.')
+        return redirect('home')
+    
+def delete_staff_address(request, pk):
+    pass
+
+def add_staff_address(request):
+    pass
+
+#-- Staff Phones --#    
+def view_staff_phones(request):
+    if request.user.is_authenticated:
+        cities = City.objects.all()
+        return render(request, 'cities.html', {'cities': cities})
+
+def view_staff_phone(request, pk):
+    if request.user.is_authenticated:
+        #look up city
+        city = City.objects.get(CITYCODE=pk)
+        return render(request, 'city_details.html', {'city':city})
+    else:
+        messages.success(request, 'You must be logged in to view this page.')
+        return redirect('home')
+    
+def delete_staff_phone(request, pk):
+    pass
+
+def add_staff_phone(request):
+    pass
+    
+#-- Staff --#    
+def view_staff(request):
+    if request.user.is_authenticated:
+        cities = City.objects.all()
+        return render(request, 'cities.html', {'cities': cities})
+
+def view_staff_member(request, pk):
+    if request.user.is_authenticated:
+        #look up city
+        city = City.objects.get(CITYCODE=pk)
+        return render(request, 'city_details.html', {'city':city})
+    else:
+        messages.success(request, 'You must be logged in to view this page.')
+        return redirect('home')
+    
+def delete_staff_member(request, pk):
+    pass
+
+def add_staff_member(request):
+    pass
+
+#-- Stretch --#    
+def view_stretches(request):
+    if request.user.is_authenticated:
+        cities = City.objects.all()
+        return render(request, 'cities.html', {'cities': cities})
+
+def view_stretch(request, pk):
+    if request.user.is_authenticated:
+        #look up city
+        city = City.objects.get(CITYCODE=pk)
+        return render(request, 'city_details.html', {'city':city})
+    else:
+        messages.success(request, 'You must be logged in to view this page.')
+        return redirect('home')
+    
+def delete_stretch(request, pk):
+    pass
+
+def add_stretch(request):
+    pass
